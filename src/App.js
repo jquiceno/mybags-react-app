825f79fcd866cd7a10a8335b4aab5edec6d9a9cf
Jquiceno <loncuster@gmail.com>
2021-04-24 12:40:14 +0200

    React router added, views project added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import { GlobalStyles } from './GlobalStyles'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Home, NewPerson } from './views'
 
 function App() {
   return (
-    <div>
+    <>
       <GlobalStyles />
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </div>
+      <BrowserRouter>
+        <Route exact path = "/" component = {Home} />
+        <Route path = "/new" component = {NewPerson} />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+import home from './Home'
+import newPerson from './NewPerson'
+
+export const Home = home
+export const NewPerson = newPerson
